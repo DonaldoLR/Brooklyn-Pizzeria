e3579dfd3d5288a755404506be9c3c31c5841fac
github username <github email address>
2021-07-11 05:51:14 -0400

    added hamburger library, updated mobile menu navigation

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,9 @@
+import './app.css';
 import Navigation from './Components/Navigation/Navigation';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPizzaSlice } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPizzaSlice);
 
 function App() {
   return (
--- src/App/Components/Navigation/Navigation.jsx
+++ src/App/Components/Navigation/Navigation.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './navigation.scss';
 import Logo from '../../Assets/Vincent_Logo.svg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 const Navigation = () => {
+  const [displayMobileMenu, setDisplayMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    setDisplayMobileMenu(!displayMobileMenu);
+  };
+  const toggleMenuStyle = {
+    display: displayMobileMenu ? 'flex' : 'none',
+  };
   return (
     <header>
-      <nav class='navbar navbar-dark bg-dark'>
-        <div class='container'>
-          <a class='navbar-brand' href='#'>
-            <img src={Logo} alt='' width='120' height='67.5' />
-          </a>
+      <nav>
+        <button
+          className={
+            displayMobileMenu
+              ? 'nav-hamburger hamburger hamburger--collapse is-active'
+              : 'nav-hamburger hamburger hamburger--collapse'
+          }
+          type='button'
+          onClick={toggleMenu}
+        >
+          <span className='hamburger-box'>
+            <span className='hamburger-inner'></span>
+          </span>
+        </button>
+        <div className='nav-logo'>
+          <img src={Logo} alt='Logo' width='120px' height='67.5px' />
+        </div>
+        <div className='nav-cart'>
+          <FontAwesomeIcon icon='pizza-slice' size='2x' color='white' />
+        </div>
+        <div className='nav-links' style={toggleMenuStyle}>
+          <ul>
+            <li className='nav-item'>Home</li>
+            <li className='nav-item'>Menu</li>
+            <li className='nav-item'>Order</li>
+            <li className='nav-item'>Contact</li>
+          </ul>
         </div>
       </nav>
     </header>
